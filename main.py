74196ddc192f9650d3236c0359a1d7f603b48315
Pranav Kashid <pranavkashid08@gmail.com>
2026-05-23 12:23:24 +0530

    marathi hindi voice added

--- main.py
+++ main.py
@@ -45,7 +45,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://voxlibro.onrender.com",
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_origin_regex=r"http://localhost(:[0-9]+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
